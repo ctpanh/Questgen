0686b8a41da4f7e0370b5dca4b1b2b6bcceaad92
Anh Cao <ctpanh03@gmail.com>
2023-07-26 16:16:31 +0700

    add questgen

---
+++ server/core/Questgen/__init__.py
@@ -0,0 +1,5 @@
+# Constructor for questgen
+from __future__ import absolute_import
+from core.Questgen.encoding import encoding
+from core.Questgen.mcq import mcq
+from core.Questgen.main import QGen, BoolQGen, AnswerPredictor
